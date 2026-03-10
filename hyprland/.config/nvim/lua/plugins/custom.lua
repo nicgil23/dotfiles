c75dbcd03922ea093fc643da04d714c0bae3a090
nicgil23 <nicgil@ucm.es>
2026-03-10 20:31:21 +0000

    Añadido NeoVim Catppuccin Mocha.

---
+++ hyprland/.config/nvim/lua/plugins/custom.lua
@@ -0,0 +1,20 @@
+return {
+  -- add catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+    },
+  },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  }
+}
