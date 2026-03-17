f66341a08b24da8160f64e29598d4e6a151e9d72
nicgil23 <nicgil@ucm.es>
2026-03-17 17:47:44 +0100

    Modificación final del waybar: por defecto ahora es visible todo lo que es necesario. Ahora se pueden cambiar posición de ventanas de manera dinámica con shortcuts (fancy shit). MUY IMPORTANTE: se ha cambiado la hora de formato de gilipollas (AM y PM) a formato normal.

--- hyprland/.config/nvim/lua/plugins/custom.lua
+++ hyprland/.config/nvim/lua/plugins/custom.lua
@@ -1,20 +1,30 @@
 return {
-  -- add catppuccin
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      flavour = "mocha",
-      transparent_background = true,
+    -- add catppuccin
+    {
+        "catppuccin/nvim",
+        lazy = true,
+        name = "catppuccin",
+        opts = {
+            flavour = "mocha",
+            transparent_background = true,
+        },
     },
-  },
 
-  -- Configure LazyVim to load catppuccin
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
+    -- Configure LazyVim to load catppuccin
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin",
+        },
     },
-  }
+
+    "saghen/blink.cmp",
+    opts = {
+        keymap = {
+            preset = "enter",
+            ["<Tab>"] = { "select_next", "fallback" },
+            ["<S-Tab>"] = { "select_prev", "fallback" },
+        },
+    },
 }
+
